fe4b9f61691523d47b861a09853350f69744907f
hamza4091 <mh382942@gmail.com>
2023-01-06 13:41:54 +0500

    second

--- api.py
+++ api.py
@@ -7,6 +7,9 @@ import json
 import base64
 import os
 import cv2
+import base64
+
+
 
 
 @app.route('/get', methods=['GET'])
@@ -61,7 +64,7 @@ def login():
         rows = cursor.fetchall()
         conn.commit()
         resp = json.dumps(rows, indent=4, sort_keys=True, default=str)
-        response = jsonify(message=rows, id=cursor.lastrowid)
+        response = jsonify(rows[0][0])
         # response.data = cursor.lastrowid
         response.status_code = 200
         return response
@@ -76,11 +79,25 @@ def signup():
         name = request.json['name']
         password = request.json['password']
         designation = request.json['designation']
+        try:
+            email = request.json['email']
+            image = request.json['image']
+        except:
+            print("An exception occurred")
+            email = "Null"
+            image = "Null"
+
         conn = mysql.connect()
         cursor = conn.cursor()
-        insert_user_cmd = """INSERT INTO signup_data (name, password, designation)
-                                           VALUES(%s, %s, %s)"""
-        cursor.execute(insert_user_cmd, (name, password, designation))
+        if email =="Null":
+            insert_user_cmd = """INSERT INTO signup_data (name, password, designation)
+                                                      VALUES(%s, %s, %s)"""
+            cursor.execute(insert_user_cmd, (name, password, designation))
+        else:
+            insert_user_cmd = """INSERT INTO signup_data (name, password, designation,email,image)
+                                                      VALUES(%s, %s, %s,%s,%s)"""
+            cursor.execute(insert_user_cmd, (name, password, designation, email,image))
+
         conn.commit()
         response = jsonify(message='User added successfully.', id=cursor.lastrowid)
         # response.data = cursor.lastrowid
@@ -88,6 +105,48 @@ def signup():
         return response
     else:
         return 'Content-Type not supported!'
+
+
+@app.route('/saveAlerts/<string:name>/<int:status>', methods=['POST'])
+def Alertdata(name, status):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    # insert_user_cmd = """insert into Alerts values(%s,%s)"""
+    cursor.execute("insert into alerts (name,status) values ('" + (name) + "' ,'" + str(status) + "')")
+    cursor.commit()
+    response = ('data added successfully.')
+    # response.data = cursor.lastrowid
+    return response
+
+
+@app.route('/getNames', methods=['GET'])
+def getAlertData():
+    conn = mysql.connect()
+    d = []
+    cur = conn.cursor()
+    cur.execute("select * from alerts")
+    rows = cur.fetchall()
+    i = 1
+    for row in rows:
+        l = {'name': row[0], 'status': row[1].strip()}
+        d.append(l)
+        i += 1
+    # resp = json.dumps(d,default=str)
+    # resp=json.loads(resp)
+    return jsonify(d)
+
+
+@app.route('/getAlertVisitor/<string:name>', methods=['GET'])
+def getAlertVisitor(name):
+    conn = mysql.connect()
+    d = []
+    cur = conn.cursor()
+    cur.execute("select * from visitor_data where name='" + (name) + "'")
+    rows = cur.fetchall()
+    for row in rows:
+        l = {'name': row[0], 'cnic': row[1], 'phone': row[2], 'destination': row[3],"date":row[4],"image": base64.b64encode(row[6]).decode()}
+        d.append(l)
+    return jsonify(d)
 
 
 if __name__ == "__main__":
